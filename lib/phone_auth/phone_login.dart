2a225964b89b66bf22532c63966087b22893e462
sapatevaibhav <sapatevaibhav@duck.com>
2024-07-10 00:22:08 +0530

    new fresh login page

--- lib/phone_auth/phone_login.dart
+++ lib/phone_auth/phone_login.dart
@@ -1,11 +1,11 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'dart:developer';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_demo/home.dart';
-import 'package:flutter_firebase_demo/phone_auth/otp_page.dart';
+import '../profile_options/profile_page.dart';
 
 class PhoneLoginPage extends StatefulWidget {
   const PhoneLoginPage({super.key});
@@ -16,24 +16,24 @@ class PhoneLoginPage extends StatefulWidget {
 
 class _PhoneLoginPageState extends State<PhoneLoginPage> {
   TextEditingController phoneController = TextEditingController();
+  TextEditingController otpController = TextEditingController();
+  bool isOTPSent = false;
+  bool isOTPValid = false;
+  String? verificationId;
 
   void sendOTP() async {
     String phone = "+91${phoneController.text.trim()}";
-    if (phone == "") {
+    if (phone == "+91") {
       log("Enter the details");
     } else {
       try {
         await FirebaseAuth.instance.verifyPhoneNumber(
           phoneNumber: phone,
           codeSent: (verificationId, resendToken) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => OTPPage(
-                  verificationId: verificationId,
-                ),
-              ),
-            );
+            setState(() {
+              isOTPSent = true;
+              this.verificationId = verificationId;
+            });
           },
           verificationCompleted: (credential) {},
           verificationFailed: (ex) {
@@ -48,46 +48,136 @@ class _PhoneLoginPageState extends State<PhoneLoginPage> {
     }
   }
 
+  void verifyOTP() async {
+    String otp = otpController.text.trim();
+    if (otp.isEmpty || verificationId == null) {
+      log("Enter the OTP");
+    } else {
+      try {
+        PhoneAuthCredential credential = PhoneAuthProvider.credential(
+          verificationId: verificationId!,
+          smsCode: otp,
+        );
+        UserCredential userCredential =
+            await FirebaseAuth.instance.signInWithCredential(credential);
+        if (userCredential.user != null) {
+          log("sign in success");
+          checkProfile(userCredential.user!.uid);
+        }
+      } on FirebaseAuthException catch (e) {
+        log(e.code.toString());
+      }
+    }
+  }
+
+  void checkProfile(String uid) async {
+    // Check if user profile exists in Firestore
+    final userDoc = await FirebaseFirestore.instance.collection('users').doc(uid).get();
+    if (userDoc.exists) {
+      // Navigate to home screen if profile exists
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+    } else {
+      // Navigate to profile page if profile does not exist
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ProfilePage(uid: uid)));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            TextField(
-              controller: phoneController,
-              decoration: const InputDecoration(
-                labelText: 'Phone',
+      backgroundColor: Colors.black,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  const Center(
+                    child: Column(
+                      children: [
+                        Icon(Icons.person, size: 64, color: Colors.black),
+                        SizedBox(height: 8.0),
+                        Text(
+                          'आपले गाव',
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  TextField(
+                    controller: phoneController,
+                    decoration: const InputDecoration(
+                      labelText: 'फोन नंबर',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: sendOTP,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                    child: const Text('OTP पाठवा'),
+                  ),
+                  const SizedBox(height: 16.0),
+                  if (isOTPSent)
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        TextField(
+                          controller: otpController,
+                          decoration: const InputDecoration(
+                            labelText: 'OTP',
+                            border: OutlineInputBorder(),
+                          ),
+                          onChanged: (value) {
+                            setState(() {
+                              isOTPValid = value.length == 6;
+                            });
+                          },
+                        ),
+                        const SizedBox(height: 16.0),
+                        ElevatedButton(
+                          onPressed: isOTPValid ? verifyOTP : null,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                isOTPValid ? Colors.black : Colors.grey,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                          ),
+                          child: const Text('लॉगिन करा'),
+                        ),
+                      ],
+                    ),
+                ],
               ),
             ),
-            const SizedBox(height: 16.0),
-
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                sendOTP();
-              },
-              child: const Text('Send OTP'),
-            ),
-            // const SizedBox(height: 16.0),
-            // TextButton(
-            //   onPressed: () {
-            //     Navigator.push(
-            //       context,
-            //       MaterialPageRoute(builder: (context) => const OTPPage()),
-            //     );
-            //   },
-            //   child: const Text('Create an account'),
-            // ),
-          ],
+          ),
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: PhoneLoginPage(),
+  ));
 }
